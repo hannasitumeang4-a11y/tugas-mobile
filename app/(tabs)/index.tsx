2327cc5e3973aceeb8276513e840fd2a60a469be
Hanna Situmeang <hannasitumeang4@gmail.com>
2026-02-17 20:39:03 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,34 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={require('../../assets/foto.jpg')}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>Tugas Mobile Programming</Text>
+      <Text>Nama: Hanna Situmeang</Text>
+      <Text>NIM: 123456</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+    borderRadius: 75,
+  },
+});
